166b40c8ca28703ddcd37472e3ad83ed6ffb9e3e
Sadi Yumuşak <sadiyumusak@gmail.com>
2019-12-03 20:03:30 +0300

    Improved EXIF-Data script

--- scripts/EXIF-Data.sh
+++ scripts/EXIF-Data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Use exiftool to view metadata of the selected audio, video or image file(s).
-# Requires zenity and exiftool
-#
+# Use exiftool to retrieve and dillo to view metadata of the selected audio, video or image file(s).
+# Requires zenity, dillo and exiftool
+# Viewer may also be zenity instead of dillo displaying exif data in html format
 OLDIFS=$IFS
 IFS="
 "
@@ -18,10 +18,16 @@ if [ ! -s "$LOGFILE" ]
 then
 	zenity --error --text="File format not supported!"
 else
-	zenity --text-info --title="$TITLE EXIF Data (Read Only)" --filename="$LOGFILE" --width=550 --height=450
-#
-# remove temp file when the zenity window is closed
+#	zenity --text-info --title="$TITLE EXIF Data (Read Only)" --filename="$LOGFILE" --width=550 --height=450
+cat "$LOGFILE" | \
+sed -e "s/^/<tr><td valign=top>/g" -e "s/$/<\/td><\/tr>/g" -e "s/\s\s*: /<\/td><td valign=top>/g" | \
+sed '1i\
+<html>\n<head>\n<title>EXIF Data</title>\n</head>\n<body>\n<table border=0>' | \
+sed -e '$a</body></html>' > "$LOGFILE.html"
+dillo -f "$LOGFILE.html"
+# remove temp file(s)
 rm -f "$LOGFILE"
+rm -f "$LOGFILE.html"
 fi
 done
 IFS=$OLDIFS
